3dba0fa6241be4d089f4bcb90687b6197ed19215
Emanuel Vogt <contatoemanuel3@gmail.com>
2025-01-22 19:17:15 -0300

    feat(domain): add additional properties to Room entity

--- src/domain/entities/Room.ts
+++ src/domain/entities/Room.ts
@@ -2,6 +2,15 @@ export class Room {
   constructor(
     public readonly id: string,
     public name: string,
-    public capacity: number
+    public capacity: number,
+    public numberOfBeds: number,
+    public stars: number,
+    public pricePerNight: number,
+    public availability: Availability[]
   ) {}
+}
+
+export interface Availability {
+  date: Date;
+  isAvailable: boolean;
 }
