9a02782408aefd536ad15e47771035f414681cee
Emanuel Vogt <contatoemanuel3@gmail.com>
2025-01-22 23:00:48 -0300

    refactor(main): implement routes with adapter

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,29 +1,39 @@
 import express from "express";
-import { RoomsController } from "../infrastructure/http/controllers/RoomsController";
-import { BookingsController } from "../infrastructure/http/controllers/BookingsController";
 import { ListAvailableRoomsUseCase } from "../application/use-cases/ListAvailableRoomsUseCase";
 import { CreateBookingUseCase } from "../application/use-cases/CreateBookingUseCase";
+import { InMemoryRoomRepository } from "../infrastructure/repositories/InMemoryRoomRepository";
+import { InMemoryBookingRepository } from "../infrastructure/repositories/InMemoryBookingRepository";
+import { DeleteBookingUseCase } from "../application/use-cases/DeleteBookingUseCase";
+import { adapterRoute } from "./adapters/express-adapter/express-route-adapter";
+import { ListRoomsController } from "../presentation/http/controllers/ListRoomsController";
+import { CreateBookingController } from "../presentation/http/controllers/CreateBookingController";
+import { DeleteBookingController } from "../presentation/http/controllers/DeleteBookingController";
 
 const app = express();
 app.use(express.json());
 
-const listAvailableRoomsUseCase = new ListAvailableRoomsUseCase();
-const createBookingUseCase = new CreateBookingUseCase();
+const roomRepository = new InMemoryRoomRepository();
+const bookingRepository = new InMemoryBookingRepository(roomRepository);
 
-const roomsController = new RoomsController(listAvailableRoomsUseCase);
-const bookingsController = new BookingsController(createBookingUseCase);
+const listAvailableRoomsUseCase = new ListAvailableRoomsUseCase(roomRepository);
+const createBookingUseCase = new CreateBookingUseCase(
+  bookingRepository,
+  roomRepository
+);
+const deleteBookingUseCase = new DeleteBookingUseCase(bookingRepository);
 
-app.get("/", async (req, res) => {
-  await roomsController.list(req, res)
-});
-
-app.get("/rooms", async (req, res) => {
-  await roomsController.list(req, res)
-});
-
-app.post("/bookings", async (req, res) => {
-  await bookingsController.create(req, res)
-});
+app.get(
+  "/rooms",
+  adapterRoute(new ListRoomsController(listAvailableRoomsUseCase))
+);
+app.post(
+  "/bookings",
+  adapterRoute(new CreateBookingController(createBookingUseCase))
+);
+app.delete(
+  "/bookings/:id",
+  adapterRoute(new DeleteBookingController(deleteBookingUseCase))
+);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
