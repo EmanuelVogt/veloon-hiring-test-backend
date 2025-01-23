95150a4ec7ac71d966fb6f727ffce83891110a50
Emanuel Vogt <contatoemanuel3@gmail.com>
2025-01-23 11:05:03 -0300

    refactor(repository): remove unnecessary method from booking repository

--- src/infrastructure/repositories/InMemoryBookingRepository.ts
+++ src/infrastructure/repositories/InMemoryBookingRepository.ts
@@ -2,61 +2,42 @@
 
 import { IBookingRepository } from "../../domain/repositories/IBookingRepository";
 import { Booking } from "../../domain/entities/Booking";
-import { bookings as initialBookings } from "../fake-data/bookings";
 import { IRoomRepository } from "../../domain/repositories/IRoomRepository";
 
 export class InMemoryBookingRepository implements IBookingRepository {
   private bookings: Booking[] = [];
-  private roomRepository: IRoomRepository;
 
-  constructor(roomRepository: IRoomRepository) {
-    this.roomRepository = roomRepository;
-    this.loadBookings();
-  }
-
-  private  loadBookings(): void {
-    initialBookings.forEach(async booking => {
-      const room = await this.roomRepository.findById(booking.room.id);
-      if (room) {
-        this.bookings.push(new Booking(
-          booking.id,
-          room,
-          new Date(booking.startDate),
-          new Date(booking.endDate)
-        ));
-      } else {
-        console.warn(`Room with ID ${booking.room.id} not found for booking ${booking.id}`);
-      }
-    });
-  }
-
-  async create(booking: Booking): Promise<void> {
-    this.bookings.push(booking);
-  }
+  constructor(private roomRepository: IRoomRepository) {}
 
   async findAll(): Promise<Booking[]> {
     return this.bookings;
   }
 
   async findById(id: string): Promise<Booking | undefined> {
-    return this.bookings.find(booking => booking.id === id);
+    return this.bookings.find((booking) => booking.id === id);
   }
 
-  async update(updatedBooking: Booking): Promise<void> {
-    const index = this.bookings.findIndex(booking => booking.id === updatedBooking.id);
-    if (index !== -1) {
-      this.bookings[index] = updatedBooking;
-    } else {
+  async create(booking: Booking): Promise<void> {
+    this.bookings.push(booking);
+  }
+
+  async update(booking: Booking): Promise<void> {
+    const index = this.bookings.findIndex((b) => b.id === booking.id);
+    if (index === -1) {
       throw new Error("Booking not found");
     }
+    this.bookings[index] = booking;
   }
 
   async delete(id: string): Promise<void> {
-    const index = this.bookings.findIndex(booking => booking.id === id);
-    if (index !== -1) {
-      this.bookings.splice(index, 1);
-    } else {
+    const index = this.bookings.findIndex((b) => b.id === id);
+    if (index === -1) {
       throw new Error("Booking not found");
     }
+    this.bookings.splice(index, 1);
+  }
+
+  async clear(): Promise<void> {
+    this.bookings = [];
   }
 }
