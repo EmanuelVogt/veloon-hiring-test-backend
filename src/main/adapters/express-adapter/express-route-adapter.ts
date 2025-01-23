6d79a6be33fd493d161a8d0c3535167e43840471
Emanuel Vogt <contatoemanuel3@gmail.com>
2025-01-22 21:15:35 -0300

    feat(main): create express router adapter

---
+++ src/main/adapters/express-adapter/express-route-adapter.ts
@@ -0,0 +1,19 @@
+import { Controller } from '../../../presentation/protocols/Controller'
+import { Request, Response } from 'express'
+
+export const adapterRoute = (controller: Controller) => {
+  return async (request: Request, response: Response) => {
+    const req = {
+      ...(request.body || {}),
+      ...(request.params || {})
+    }
+    const httpResponse = await controller.handle(req)
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+      response.status(httpResponse.statusCode).json(httpResponse.body)
+    } else {
+      response.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message
+      })
+    }
+  }
+}
